34d1f2bab68e16add380b09c2425720a26508c28
Martin Reifschneider <mreifschneider@me.com>
2025-02-26 10:56:40 +0100

    Create start-screen, game components

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen',
+  imports: [],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+}
